8bc237f4c5aee5e7d4dd847da457ecaa9f4bf86f
David Reinisch <david.reinisch@tastenwerk.com>
2014-03-28 15:29:38 +0100

    works with strings

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  * @Date:   2014-03-28 13:39:44
  *
  * @Last Modified by:   David Reinisch
- * @Last Modified time: 2014-03-28 13:40:12
+ * @Last Modified time: 2014-03-28 15:27:10
  *
  * This source code is not part of the public domain
  * If server side nodejs, it is intendet to be read by
@@ -18,6 +18,75 @@
 (function () {
 
   var bodyParser = {};
+
+  bodyParser.parseString = function parseString( string, options ){
+    setOptions( options ); // BIG TODO
+    return parse( string );
+  };
+
+  function setOptions( options ){
+    if( options ){
+      var keyBracketStart = options.keyBracketStart ? options.keyBracketStart : '[';
+      var keyBracketEnd  = options.keyBracketEnd   ? options.keyBracketEnd   : ']';
+      var valueDelimiter  = options.valueDelimiter  ? options.valueDelimiter  : '=';
+      var delimiter       = options.delimiter       ? options.delimiter       : ',';
+    }
+  }
+
+  function parse( string ){
+    var params = getArray( string, '&');
+    var root = {};
+
+    params.forEach( function ( keyAndValue ){
+      var splitted = keyAndValue.split('=');
+      var key = unescape( splitted[0] );
+      var value = unescape( splitted[1] );
+      addKeyAndValue( root, key, value );
+    });
+
+    return root;
+  }
+
+  function addKeyAndValue( rootObject, key, value ){
+    console.log(key);
+    var keys = getKeys( key );
+    var obj = rootObject;
+    keys.forEach( function( keyName, index ){
+      if( !obj[keyName] )
+        obj[keyName] = {};
+      if( index + 1 === keys.length )
+        obj[keyName] = value;
+      else
+        obj = obj[keyName];
+    });
+  }
+
+  function parseKeys( value ){
+    var keys = value.match(/\[[^\[\]]*\]/g);
+      console.log('last', keys);
+    keys.forEach( function( key, index ){
+      keys[index] = key.replace('[','').replace(']','');
+    });
+
+    return keys;
+  }
+
+  function getKeys( values ){
+    var keys = [];
+    if( values.indexOf("[") > 0 ){
+      var first = values.substring( 0, values.indexOf("[") );
+      var last = values.substring( values.indexOf("["), values.length+1 );
+      keys.push( first );
+      keys = keys.concat( parseKeys( last ) );
+    } else
+      keys.push( values );
+
+    return keys;
+  }
+
+  function getArray( string, _delimiter ){
+    return string.split( _delimiter );
+  }
 
   
 
--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@
  * @Date:   2014-03-28 13:41:44
  *
  * @Last Modified by:   David Reinisch
- * @Last Modified time: 2014-03-28 13:54:36
+ * @Last Modified time: 2014-03-28 15:10:39
  *
  * This source code is not part of the public domain
  * If server side nodejs, it is intendet to be read by
@@ -19,7 +19,7 @@ describe( '> CGI-BODY-PARSER  TEST <', function(){
   var parser = require('../index.js');
 
   it('parses', function(){
-    console.log('here', parser);
+    console.log('here', parser.parseString('d=test&y%5Bh%5D%5Bl%5D=sd%5D&y%5Bh%5D%5Bk%5D=123&y%5Bf%5D=%26quotjjj', {}));
     
   });
 
