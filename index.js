286a5bb3c19f9ef29cd5fcf7aeba7c3aa98b858d
David Reinisch <david.reinisch@tastenwerk.com>
2014-03-28 13:50:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/** 
+ * @Author: David Reinisch
+ * @Company: TASTENWERK e.U.
+ * @Copyright: 2014 by TASTENWERK
+ * @License: Commercial
+ *
+ * @Date:   2014-03-28 13:39:44
+ *
+ * @Last Modified by:   David Reinisch
+ * @Last Modified time: 2014-03-28 13:40:12
+ *
+ * This source code is not part of the public domain
+ * If server side nodejs, it is intendet to be read by
+ * authorized staff, collaborator or legal partner of
+ * TASTENWERK only
+ */
+
+(function () {
+
+  var bodyParser = {};
+
+  
+
+  module.exports = bodyParser;
+
+}());
---
+++ test/test.js
@@ -0,0 +1,23 @@
+/** 
+ * @Author: David Reinisch
+ * @Company: TASTENWERK e.U.
+ * @Copyright: 2014 by TASTENWERK
+ * @License: Commercial
+ *
+ * @Date:   2014-03-28 13:41:44
+ *
+ * @Last Modified by:   David Reinisch
+ * @Last Modified time: 2014-03-28 13:41:44
+ *
+ * This source code is not part of the public domain
+ * If server side nodejs, it is intendet to be read by
+ * authorized staff, collaborator or legal partner of
+ * TASTENWERK only
+ */
+
+(function( App ){
+  
+
+
+  
+})(App);
